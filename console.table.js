230eebffde6b250fa506bba36454d0bf38fc8a11
sirwan <sirwan.afifi@gmail.com>
2017-03-11 00:00:22 +0330

    intial commit

---
+++ console.table.js
@@ -0,0 +1,27 @@
+'use strict';
+
+console.table = function (arr) {
+
+    let keys = Object.keys(arr[0]);
+
+    const horizontalLine = '\r\n\x1b[34m' + '-'.repeat(12 * keys.length) + '\r\n\x1b[0m';
+
+    let heading = '\x1b[35m' + Object.keys(arr[0]).join('\t') + '\x1b[0m';
+
+    let str = horizontalLine;
+
+    str += heading;
+
+    arr.forEach(function (obj) {
+
+        str += horizontalLine + Object.values(obj).join('\t\t');
+
+    });
+
+    str += horizontalLine;
+
+    console.log(str);
+
+};
+
+module.exports = console.table;
---
+++ index.js
@@ -0,0 +1,8 @@
+require('./console.table');
+
+let arr = [
+    { firstName: 'Sirwan', lastName: 'Afifi', age: 27 },
+    { firstName: 'Person2', lastName: 'P2', age: 32 }
+];
+
+console.table(arr);
